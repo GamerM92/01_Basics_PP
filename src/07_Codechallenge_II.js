9179a1eb4bf78501ca177f23c1f7335b45afe055
GamerM92 <manuel_marx@outlook.de>
2025-01-07 14:17:59 +0000

    Korrektur (Zeile 51)

--- src/07_Codechallenge_II.js
+++ src/07_Codechallenge_II.js
@@ -16,20 +16,61 @@ Alter 13-17 // Cola
 
 /* Getränke-Challenge */
 
+// console.log("")
+// console.log("--------------------")
+// console.log("LADE GETRÄNKE CHALLENGE")
+// console.log("--------------------")
+// console.log("GETRÄNKE CHALLENGE GELADEN")
 const prompt = require('prompt-sync')({sigint: true});
-
-const name = prompt("Gib einen Namen ein: ");
-let age = Number(prompt("Gib einen Beruf ein: "));
-
-
-if (age > 0 & age < 6);
-{
-    console.log(name + " trinkt Milch");
+// console.log("")
+const name = prompt("Bitte gib einen Namen ein: ");
+if (/^[a-zA-Z]+(-[a-zA-Z]+[a-zA-Z])*$/.test(name)) {
+    
+} else {
+    console.log("")
+    console.log("Der name '" + name + "' enthält ungültige Zeichen");
+    console.log(""); console.log("--------------------")
+    return;
 }
-// else if (age < 12);
-// {
-//     console.log(name + " trinkt Saft");
-// }
+let age = Number(prompt("Gib das Alter von " + name + " ein: "));
+console.log("")
+console.log("")
+if (isNaN(age))
+{
+    console.log("Der Wert für das Alter ist ungültig")
+    console.log(""); console.log("--------------------")
+}
+else{
+    switch (isNaN(age) != true) 
+    {
+        case age <= 0:
+            console.log(name + " ist " + age + " Jahre alt? Wie geht das denn?");
+            console.log(""); console.log("--------------------")
+        break;
 
+        case age > 0 && age <= 5:
+            console.log(name + " ist " + age + " Jahre alt und darf Milch trinken");
+            console.log(""); console.log("--------------------")
+        break;
+    
+        case age >= 6 && age <= 12:
+            console.log(name + " ist " + age + " Jahre alt und darf Milch und Saft trinken");
+            console.log(""); console.log("--------------------")
+        break;
 
+        case age >= 13 && age <= 17:
+            console.log(name + " ist " + age + " Jahre alt und darf Milch, Saft und Cola trinken");
+            console.log(""); console.log("--------------------")
+        break;
 
+        case age >= 18 && age <= 90:
+            console.log(name + " ist " + age + " Jahre alt und darf Milch, Saft, Cola und Wein trinken");
+            console.log(""); console.log("--------------------")
+        break;
+    
+        default:
+        console.log("Die Zahl liegt außerhalb der Bereiche");
+        console.log(""); console.log("--------------------")
+    }
+
+}
