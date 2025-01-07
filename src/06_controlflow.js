6c447a92ec4ce6c04a1e5299151705f6b7e60927
GamerM92 <manuel_marx@outlook.de>
2025-01-07 09:49:52 +0000

    Altersvergleich

---
+++ src/06_controlflow.js
@@ -0,0 +1,39 @@
+// Entscheidungsstrukturen | control flow
+
+// Deklaration + Assignment
+const ageJohn = 30;
+const ageMark = 30;
+
+// Deklaration
+let isJohnOlder, isJohnEqual;
+
+// Test | Logische Aussage
+isJohnOlder = (ageJohn > ageMark);
+isJohnEqual = (ageJohn == ageMark);
+
+// Ausgabe
+console.log("------------------");
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
+console.log("------------------");
+
+/************ IF  ************/
+// TINA --> There is no alternative!
+// entweder JA oder nix ... alternativlos
+// Türsteher-Politik (Doors 10 p.m.)
+
+if (isJohnOlder)
+{
+    console.log("John ist älter!");
+}
+else if (isJohnEqual)
+{
+    console.log("John ist so alt wie Mark!");
+}
+else
+{
+    console.log("Mark ist älter!")
+}
+console.log("------------------");
