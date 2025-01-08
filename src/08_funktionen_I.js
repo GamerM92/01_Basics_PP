4158e51900723d4b2812c9448b2b564fbed68ea7
GamerM92 <manuel_marx@outlook.de>
2025-01-08 10:11:05 +0000

    08 / Parameter & Trennen der Funktionalitäten

--- src/08_funktionen_I.js
+++ src/08_funktionen_I.js
@@ -2,16 +2,66 @@
 // 1. Kapselung von Codeblöcken
 
 // Funktionsaufruf | call
-
-const prompt = require('prompt-sync')({sigint: true});
-console.log("")
-const name = prompt("Bitte gib einen Namen ein: ");
-test();
+// test();
 
 // Funktionsrumpf | callee
 // Funktionsdekleration
 function test()
 {
-    console.log("Hallo, " + name + "!")    
+    console.log("Hallo, Peter!")    
+}
+
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+// ausgabeNamen(); // call
+
+function ausgabeNamen() {
+    // interne Variable | What happe nd in VEGAS
+    let firstName = "Peter"
+    console.log("Hallo, " + firstName + "!");
+}
+
+// console.log("Hallo, " + firstName + "!"); // Fehler : scope!
+
+/***** Funktionen 02b *****/
+// 2b. Parametrisierung + Datenübergabe von AUSSEN
+
+// ausgabeNamen2("Falk")
+// ausgabeNamen2("Peter")1
+// ausgabeNamen2("Maria")
+
+function ausgabeNamen2(firstName) {
+    console.log("Hallo, " + firstName + "!"); 
+}
+
+/***** Funktionen 02c *****/
+// 2c. Mehrere Parameter / Argumente
+
+
+// const prompt = require('prompt-sync')({sigint: true});
+// ausgabeNamenParams(prompt("Vorname: "),prompt("Nachname:")); // Piping
+// ausgabeNamenParams("Max","Mütze")
+
+// function ausgabeNamenParams(firstName, secondName) {
+//     console.log("Hallo, " + firstName + " " + secondName + "!");
+// }
+
+/***** Funktionen 03a *****/
+// 03a. Vorbereitung -Trennen der Funktionalitäten
+// Postulat: one function = one job (uncle Bob)
+// SRP single responsibility principle
+
+ausgabeNamenSRP("Max","Mütze")
+
+function ausgabeNamenSRP(firstName, secondName) {
+
+        // 1. Funktionalität: string composing
+    let GAP = " ";
+    let outputStr = "Hallo, " + firstName + " " + secondName + "!"
+
+        // 2. Funktionalität: string output
+    console.log(outputStr);
+    
 }
 
