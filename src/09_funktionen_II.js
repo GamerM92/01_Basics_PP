15e214639643f363cc5d1f050fb9da4afbf93a08
GamerM92 <manuel_marx@outlook.de>
2025-01-09 09:51:03 +0000

    09 / module (add,sub,multiply,divide)

--- src/09_funktionen_II.js
+++ src/09_funktionen_II.js
@@ -8,7 +8,53 @@
 4. Ausgabe in Konsole : 
 */
 
+const ERROR_STR_DIV = "Divison durch 0 nicht möglich";
 
+// module: division a / b | test:
+output(divide(4,2));
+output(divide(3,2));
+output(divide(3,-2));
+output(divide(0,2));
+output(divide(3,0));
+output(divide(0,0));
+function divide(a,b) {
+    
+    // if (b == 0) {
+    //     return ERROR_STR_DIV;
+    // }    
+    //     return a / b
+    
+    
+    if (b != 0) {
+        return a / b;
+    }
+        return ERROR_STR_DIV
+}
+
+// module: multiplication a * b | test:
+// output(multiply(3,2));
+// output(multiply(3,-2));
+// output(multiply(3,0));
+function multiply(a,b) {
+    return a * b;
+}
+
+// module: subtraction a - b | test:
+// output(subtract(3,2));
+// output(subtract(3,-2));
+// output(subtract(3,0));
+// output(subtract(0,2));
+function subtract(a,b) {
+    return a - b;
+}
+
+// module: addition a + b | test:
+// output(add(3,2));
+// output(add(3,-2));
+// output(add(3,0));
+function add(a,b) {
+    return a + b;
+}
 
 // module: output | test:
 // output("Hi");
