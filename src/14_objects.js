db6faf716e2274c8b5a59ce2086418a2587e4479
GamerM92 <manuel_marx@outlook.de>
2025-01-13 14:51:20 +0000

    object in object

---
+++ src/14_objects.js
@@ -0,0 +1,90 @@
+/***** Erinnerung: Arrays *******/
+
+let arr;
+// arr = ["Ich","bin","Max"];
+// output(arr);
+// output(arr[0]);
+// output("------------");
+
+// nested arrays - Prinzip: [[[]]]
+// nested arrays  - hier: [[],[]]
+// arr =   [
+//             ["Ich","bin","Max"],
+//             ["Ich","bin","Moritz"]
+//         ];
+      
+// output(arr);
+// output(arr[0]);
+// output(arr[0][0][0]+ " " + arr[1][2]);
+// output(arr[1][0]);
+// output("------------");
+
+// // nested loop
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr[i].length; j++) {
+//         output("Index i: " + i + " Index j: " + j);
+//         output(arr[i][j]);       
+//     }
+// }
+
+
+/***** Objekte 1 Daten/Funktionen *****/
+
+// let person = {
+//                 firstName: "Alexander",
+//                 familyName: "Pesch",
+//                 salary: [120000, 160000],
+//                 sayHello: function() {
+//                     return "Hallo";
+//                 },
+//                 sayHello2: function() {
+//                     return "Hallo , ich bin " + this.firstName;
+//                 }
+
+// }
+// // output(person);
+// output(person.firstName)
+// output(person.familyName)
+// output(person.salary[1])
+// output(person.sayHello());
+// output(person.sayHello2());
+// ;
+// const txt = person.sayHello() + ", ich bin " + person.firstName +
+//             " und verdiene " + person.salary[1] + " p.a."
+
+
+
+// output(txt)
+
+
+let baikal = {
+                value: "10m",
+                deep: {
+                        deeper: {
+                                    deepest: "Das Licht - auf 1642m!",
+                                    temperature: "4 Grad Celsius"
+                        }
+                }
+            };
+
+output(baikal)
+output(baikal.value)
+output(baikal.deep.deeper.deepest)
+output(baikal.deep.deeper.temperature)
+
+
+
+
+
+
+
+
+
+
+
+
+// Modul: Ausgabe | Test
+//output("hi");
+function output(outputData) {
+    console.log(outputData);
+}
